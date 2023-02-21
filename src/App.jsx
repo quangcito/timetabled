65e418c8d6a8cb4d420a1cbf38907b63eb264f18
Quang Duong Nguyen <quanguyendg@gmail.com>
2023-02-21 17:59:28 -0600

    add calendar and event component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Calendar from './components/Calendar'
 
 const App = () => {
 
   return (
     <div className="App">
-      
+      <h1>Itinerary for 7 Days in New York</h1>
+      <h2>One week in New York is wonderful. If want to get to know the city and see all the sights, then spending 7 days in New York is perfect!</h2>
+      <Calendar />
     </div>
   )
 }
